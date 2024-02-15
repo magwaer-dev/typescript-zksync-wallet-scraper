0124ec24814987366f73f0ec95ec6d6d353f4db6
Alex Mgw <egjuice99@gmail.com>
2024-02-15 20:59:30 +0200

    fix

--- src/wallet-stats/wallet-balance.ts
+++ src/wallet-stats/wallet-balance.ts
@@ -7,11 +7,13 @@ export async function getBalance(walletAddress: string, proxy?: string, userAgen
   const apiUrl = buildApiUrl(balanceApiEndpoint, params);
   const response = await makeApiRequest<AccountEtherBalanceResponseDto>(apiUrl, proxy, userAgent);
   // console.log(response);
-  console.log("Log from wallet-balance.ts/line 10:", "Using Proxy: ", proxy, "for address: ", walletAddress, "with user-agent: ", userAgent);
+  console.log("Log from wallet-balance.ts/line 10");
+  console.log("Using Proxy: ", proxy);
+  console.log("for address: ", walletAddress);
+  console.log("with user-agent: ", userAgent);
 
   if (response === null) {
-    console.error("Error: Response from balance API is null.");
-    return null;
+    throw new Error("Error: Response from balance API is null.");
   }
 
   const balanceWei = parseFloat(response.result);
