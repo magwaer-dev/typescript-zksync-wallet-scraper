f39694aeb4ce7047c8220847c22feb248c135737
Alex Mgw <egjuice99@gmail.com>
2024-03-03 06:09:05 +0200

    fix wallet bug

--- src/wallets.ts
+++ src/wallets.ts
@@ -5,16 +5,18 @@ interface Wallet {
 }
 
 export const wallets: Wallet[] = [
+  
   {
-    address: "0x4F471D378B84422A971846e85bE3792b7f0f63EA",
+    address: "0x22179a4D1cC7ECD9E326423725f15888E3514DB2",
     userAgent:
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 HUI/96.0",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Edg/98.0.1108.56",
     // proxy: "your-proxy-url if needed",
   },
   {
-    address: "0x339F40D6B7a291d1ea81C20c3a58Eae9e92b22d2",
+    address: "0x4F471D378B84422A971846e85bE3792b7f0f63EA",
     userAgent:
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Edg/98.0.1108.56",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Edg/98.0.1108.56",
+
     // proxy: "your-proxy-url if needed",
   },
 ];
